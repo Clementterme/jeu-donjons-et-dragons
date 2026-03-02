46d0d0cc0541fc3f40ec258ed4ec21880e5be3ef
Clementterme <clement.terme.simplon@gmail.com>
2026-03-02 13:40:54 +0100

    Premier menu

--- src/fr/campus/dungeoncrawler/main/Main.java
+++ src/fr/campus/dungeoncrawler/main/Main.java
@@ -1,7 +1,31 @@
 package fr.campus.dungeoncrawler.main;
 
+import java.util.Scanner;
+
 public class Main {
     static void main() {
+        Scanner sc = new Scanner(System.in);
+        int choix;
 
+        do {
+            System.out.println("Menu :");
+            System.out.println("1 - Nouveau personnage");
+            System.out.println("2 - Quitter le jeu");
+
+            choix = sc.nextInt();
+
+            switch (choix) {
+                case 1:
+                    System.out.println("Vous avez choisi : Nouveau personnage");
+                    break;
+                case 2:
+                    System.out.println("Au revoir !");
+                    break;
+                default:
+                    System.out.println("Choix invalide");
+            }
+        } while (choix != 1 && choix != 2);
+
+        sc.close();
     }
 }
