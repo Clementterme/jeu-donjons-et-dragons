0d16dfef2e0473a2e90353c4a560dbc31252d690
Clementterme <clement.terme.simplon@gmail.com>
2026-03-02 14:03:00 +0100

    Gestion des erreurs

--- src/fr/campus/dungeoncrawler/main/Main.java
+++ src/fr/campus/dungeoncrawler/main/Main.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner sc = new Scanner(System.in);
-        int choix;
+        int choix = 0;
 
         do {
             System.out.println("Menu :");
             System.out.println("1 - Nouveau personnage");
             System.out.println("2 - Quitter le jeu");
+            System.out.print("Votre choix : ");
 
-            choix = sc.nextInt();
+            if (sc.hasNextInt()) {
+                choix = sc.nextInt();
+            } else {
+                System.out.println("Veuillez entrer un nombre valide !");
+                sc.next();
+                continue;
+            }
 
             switch (choix) {
                 case 1:
